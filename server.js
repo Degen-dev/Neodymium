3dfd993360cbfdbf892f2386b36b9d736c887c89
Degenerate <82691315+Degenerate0001@users.noreply.github.com>
2021-10-13 02:32:29 +0000

    make server.js better :D

--- server.js
+++ server.js
@@ -1,26 +1,48 @@
-const http = require('http'),
-    https = require('https'),
-    fs = require('fs'),
+const
+    Corrosion = require('./Corrosion-Heroku'),
+    path = require('path'),
     config = require('./config.json'),
-    Corrosion = require('corrosion'),
+    fs = require('fs'),
+    http = require('http'),
+    https = require('https'),
     express = require('express'),
+    app = express(),
+    port = process.env.PORT || config.port,
+    /*ssl = {
+        key: fs.readFileSync(path.join(__dirname, '/ssl/default.key')),
+        cert: fs.readFileSync(path.join(__dirname, '/ssl/default.crt')),
+    },*/ 
+
+    //Uncomment all comments (excluding this one) if you would like to use https
+
+    server = http/*s*/.createServer(/*ssl, */app),
+    error = fs.readFileSync(path.normalize(__dirname + '/public/error.html'), 'utf-8'),
     proxy = new Corrosion({
         prefix: config.prefix,
-        codec: 'xor'
-    }),
-    app = express(),
-    gamermode = (req, res) => {
-        if (req.url.startsWith(config.prefix)) return proxy.request(req, res);
-        req.pathname = req.url.split('#')[0].split('?')[0];
-        req.query = {};};
-app.use('/', express.static(__dirname + '/public'));
-app.use('/', gamermode);
-// Simple HTTP server.
-app.listen(process.env.PORT || config.port, () => {console.log(`Hacker proxy running at http://localhost:${config.port}`)});
- 
-// This is the HTTPS server. (use this if you are self-hosting)
-//https.createServer({
-//	  key: fs.readFileSync('./ssl/default.key'),
-//	cert: fs.readFileSync('./ssl/default.crt')}, app)
-//	.listen(process.env.PORT || config.port, function () {console.log(`Hacker proxy running at http://localhost:${config.port}`)});
-//Remove this if self-signed SSL is a problem for you.
+        title: config.title,
+        codec: config.codec,
+        standardMiddleware: true,
+        requestMiddleware: [
+            Corrosion.middleware.blacklist((config.blacklist || []), 'This page has been blocked!'),
+        ],
+    });
+
+proxy.bundleScripts();
+
+app.get('/', function(req, res){
+    res.sendFile('index.html', { root: __dirname + '/public/'});
+});
+app.get('/media', function(req, res){
+    res.sendFile('media.html', { root: __dirname + '/public/'})
+});
+app.get('/legal', function(req, res){
+    res.sendFile('legal.txt', { root: __dirname + '/public/assets/'})
+});
+app.use(express.static(path.normalize(__dirname + '/public/')));
+app.use((req, res) => {
+    if (req.url.startsWith(proxy.prefix)) return proxy.request(req, res);
+    res.status(404, res.send(error))
+});
+
+server.on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(port);
+console.log('Neodymium is available on port ' + port);
